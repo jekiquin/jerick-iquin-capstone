0cd0e3c511594fadfbadf655500a1f4cbe7b2d9b
JekIquin <jci.dev.0@gmail.com>
2021-09-23 14:47:38 -0700

    added movement to the paddles

--- client/src/phaser/paddle-ball/scenes/GameScene.js
+++ client/src/phaser/paddle-ball/scenes/GameScene.js
@@ -1,4 +1,5 @@
 import { Scene } from 'phaser';
+import { initKeys, gameControls } from '../utils/game-controls';
 
 class GameScene extends Scene {
     constructor(){
@@ -7,15 +8,21 @@ class GameScene extends Scene {
     }
 
     preload() {
-
+        this.load.image('bg', 'assets/images/paddleboardbg.png');
+        this.load.image('paddle', 'assets/images/paddle.png');
     }
 
     create() {
-
+        this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'bg').setScale(3.5,3);
+        this.gameState.player1 = this.add.sprite(60, this.cameras.main.centerY, 'paddle').setScale(3);
+        this.gameState.player2 = this.add.sprite(this.cameras.main.displayWidth - 60, this.cameras.main.centerY, 'paddle').setScale(3);
+        
+        initKeys(this)
+        
     }
 
     update() {
-
+        gameControls(this); 
     }
 }
 
---
+++ client/src/phaser/paddle-ball/utils/game-controls.js
@@ -0,0 +1,24 @@
+// import { Input } from 'phaser';
+
+export function initKeys(scene) {
+    scene.gameState.keys = scene.input.keyboard.addKeys('W, S, up, down');
+    console.log(scene.gameState.keys)
+}
+
+export function gameControls(scene) {
+    if (scene.gameState.keys.up.isDown) {
+        scene.gameState.player2.y -= 10;
+    }
+
+    if (scene.gameState.keys.down.isDown) {
+        scene.gameState.player2.y += 10;
+    }
+
+    if (scene.gameState.keys.W.isDown) {
+        scene.gameState.player1.y -= 10;
+    }
+
+    if (scene.gameState.keys.S.isDown) {
+        scene.gameState.player1.y += 10;
+    }
+}

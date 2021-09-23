94f8be26bc9c0c269922d61c9b76e69f75ef741f
JekIquin <jci.dev.0@gmail.com>
2021-09-23 11:42:38 -0700

    initialize paddleball config

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {Route, Switch} from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import SpaceInvaders from './pages/SpaceInvaders/SpaceInvaders';
+import PaddleBall from './pages/PaddleBall/PaddleBall';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Switch>
         <Route path='/' exact component={HomePage} />  
         <Route path='/space-invaders' component={SpaceInvaders} />
+        <Route path='/paddle-ball' component={PaddleBall} />
       </Switch>
     </div>
   );

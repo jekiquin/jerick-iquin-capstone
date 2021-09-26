c75d73c9976597b87cf75618625b39a4aa924889
JekIquin <jci.dev.0@gmail.com>
2021-09-25 23:14:36 -0700

    fixed error. Issue with relative paths on image preloads

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,13 @@
 import {Route, Switch, Redirect} from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import GamePage from './pages/GamePage/GamePage';
-import SpaceInvaders from './pages/SpaceInvaders/SpaceInvaders';
-import PaddleBall from './pages/PaddleBall/PaddleBall';
-import Breakout from './pages/Breakout/Breakout';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='/games' exact component={HomePage} />
-        <Route path='/games/:gameId' component={GamePage} />  
-        {/* <Redirect from='/' to='/games' /> */}
-        {/* <Route path='/space-invaders' component={SpaceInvaders} />
-        <Route path='/pong' component={PaddleBall} />
-        <Route path='/breakout' component={Breakout} /> */}
+        <Route path='/' exact component={HomePage} />
+        <Route path='/:gameId' component={GamePage} />  
       </Switch>
     </div>
   );

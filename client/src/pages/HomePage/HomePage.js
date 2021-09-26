30975f00bb2aa7345f5a9b26e5f93f7bbf0c2c6d
JekIquin <jci.dev.0@gmail.com>
2021-09-26 06:43:41 -0700

    added arcade frame

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,15 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import './HomePage.scss';
+import arcade from '../../assets/images/arcade.png';
 
-function HomePage() {
-    return(
-        <main>
-            <ul className='game-library'>
-                <li className='game'><Link to='/space-invaders'>Space Invaders</Link></li>
-                <li className='game'><Link to='/pong'>Pong</Link></li>
-                <li className='game'><Link to='/breakout'>Breakout</Link></li>
-            </ul>
-        </main>
-    )
+
+class HomePage extends Component{
+    state = {
+        redirect: false
+    }
+    render() {
+        return(
+            <main>
+                <div className='arcade'>
+                    <img className='arcade__frame' src={arcade} alt='arcade' />
+                    <div className='arcade__imgs'>
+                        
+                    </div>
+                </div>
+                {/* <ul className='game-library'>
+                    <li className='game'><Link to='/space-invaders'>Space Invaders</Link></li>
+                    <li className='game'><Link to='/pong'>Pong</Link></li>
+                    <li className='game'><Link to='/breakout'>Breakout</Link></li>
+                </ul> */}
+            </main>
+        )
+    }
+
 }
 
 export default HomePage;

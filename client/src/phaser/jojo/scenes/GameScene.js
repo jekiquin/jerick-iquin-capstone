a55c413f8b9f8532084557755f022a5861004a04
JekIquin <jci.dev.0@gmail.com>
2021-09-26 18:13:18 -0700

    fixed layout and styling on homepage again

---
+++ client/src/phaser/jojo/scenes/GameScene.js
@@ -0,0 +1,22 @@
+import { Scene } from 'phaser';
+
+class GameScene extends Scene {
+    constructor() {
+        super({key: 'GameScene'});
+        this.gameState = {}
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update() {
+
+    }
+}
+
+export default GameScene;

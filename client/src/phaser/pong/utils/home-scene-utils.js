0d8b00064b3a9e124c54742b1ff1cefaf14c3f24
JekIquin <jci.dev.0@gmail.com>
2021-09-27 18:12:56 -0700

    updated pong homepage

--- client/src/phaser/pong/utils/home-scene-utils.js
+++ client/src/phaser/pong/utils/home-scene-utils.js
@@ -38,11 +38,12 @@ export function updateDisplay(scene, gameTime) {
         pressToPlay(scene);
     }
     scene.gameState.updatePressToPlay(gameTime);
+
 }
 
 function pressToPlay(scene) {
     scene.gameState.pressToPlay = scene.add.text(scene.cameras.main.centerX - 80, 3*scene.cameras.main.centerY/2, 'Press here to play').setOrigin(0, 0);
-    // scene.gameState.pressToPlay.setInteractive();
+    scene.gameState.pressToPlay.setInteractive();
     scene.gameState.updatePressToPlay = initPresstoPlay(scene);
 
     scene.input.on('pointerup', ()=> {

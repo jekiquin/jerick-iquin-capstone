cfd6ca1e8b311109a203cd0d06bf1ad3fa7f69ba
Jerick Iquin <jci.dev.0@gmail.com>
2023-03-22 09:23:55 -0700

    changed the logic for highscore

--- client/src/pages/GamePage/GamePage.js
+++ client/src/pages/GamePage/GamePage.js
@@ -1,4 +1,3 @@
-
 import { Component, createRef } from 'react';
 import { IonPhaser } from '@ion-phaser/react';
 import { Link } from 'react-router-dom';
@@ -7,90 +6,102 @@ import { readGameScore, writeGameScore } from '../../utils/read-game';
 import GameInstructions from '../../components/GameInstructions/GameInstructions';
 import homeButton from '../../assets/images/home.png';
 import infoButton from '../../assets/images/info.png';
-import './GamePage.scss'
+import './GamePage.scss';
 
 class GamePage extends Component {
-    state = {
-        gameInstance: null,
-        topScore: 0,
-        instructions: null
+  state = {
+    gameInstance: null,
+    topScore: 0,
+    instructions: null,
+  };
+
+  gameRef = createRef();
+
+  componentCleanup = async () => {
+    const { gameId } = this.props.match.params;
+    const { user } = this.props.location.state;
+    const { topScore } = this.state;
+    const highScore = readGameScore(this.gameRef.current);
+    // axios post here maybe?
+    if (highScore) {
+      // if there's no topscore or if it's zero, just move on.
+      gameFetcher
+        .post('/get-highscores', {
+          game: gameId,
+          name: user,
+          score: highScore,
+        })
+        .then(() => {
+          console.log('Post success!');
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
 
-    gameRef = createRef();
+    this.gameRef.current.destroy();
+  };
 
-    componentCleanup = async () => {
-        const { gameId } = this.props.match.params;
-        const {user} = this.props.location.state;
-        const {topScore} = this.state;
-        const highScore = readGameScore(this.gameRef.current);
-        // axios post here maybe?
-        if(highScore && highScore !== topScore) {
-            // if there's no topscore or if it's zero, just move on.
-            gameFetcher.post('/get-highscores', {
-                game: gameId,
-                name: user,
-                score: highScore
-            })
-            .then(() => {
-                console.log('Post success!')
-            })
-            .catch (error => {
-                console.log(error);
-            })
-        }
+  componentDidMount() {
+    const { gameId } = this.props.match.params;
+    gameFetcher.get(`/get-highscores/${gameId}`).then((response) => {
+      const gameHighScore = response.data;
+      import(`../../phaser/${gameId}/phaser-game.js`).then((response) => {
+        this.setState({
+          gameInstance: response.gameConfig,
+          instructions: response.instructions,
+        });
+        const topScore = gameHighScore ? gameHighScore[0].score : 0;
+        this.setState({ topScore });
+        setTimeout(() => {
+          // phaser needs time to generate the gamescores.
+          writeGameScore(this.gameRef.current, topScore);
+        }, 550);
+      });
+    });
+  }
 
-        this.gameRef.current.destroy();
-    }
+  componentWillUnmount() {
+    this.componentCleanup();
+  }
 
-    componentDidMount() {
-        const { gameId } = this.props.match.params;
-        gameFetcher.get(`/get-highscores/${gameId}`)
-            .then(response => {
-                const gameHighScore = response.data;
-                import(`../../phaser/${gameId}/phaser-game.js`)
-                    .then(response => {
-                        this.setState({
-                            gameInstance: response.gameConfig,
-                            instructions: response.instructions
-                        });
-                        const topScore = gameHighScore ? gameHighScore[0].score : 0;
-                        this.setState({topScore});
-                        setTimeout(() => {
-                            // phaser needs time to generate the gamescores.   
-                            writeGameScore(this.gameRef.current, topScore);
-                            
-                        }, 550)
-                    });
-            })
-    }
-
-    componentWillUnmount() {
-        this.componentCleanup();
-    }
-
-    render() {
-        const { gameInstance, instructions } = this.state;
-        return !gameInstance
-        ? <div className='gamepage'><h1>Loading...</h1></div>
-        : (
-            <div className='gamepage'>
-                <div id="phaser-game">
-                    {gameInstance && <IonPhaser ref={this.gameRef} game={gameInstance}/>}
-                </div>
-                <div className='gamepage__buttons'>
-                    <Link to='/' >
-                        <img className='gamepage__button' src={homeButton} alt='home button' />
-                    </Link>
-                    <label htmlFor="invisiblecheck">
-                        <img className='gamepage__button' src={infoButton} alt='back button' />
-                    </label>
-                    <input className='gamepage__invisiblecheck' type='checkbox' name='invisblecheck' id='invisiblecheck' />
-                    <GameInstructions instructions={instructions} />
-                </div>
-
-            </div>
-        )
-    }
+  render() {
+    const { gameInstance, instructions } = this.state;
+    return !gameInstance ? (
+      <div className="gamepage">
+        <h1>Loading...</h1>
+      </div>
+    ) : (
+      <div className="gamepage">
+        <div id="phaser-game">
+          {gameInstance && <IonPhaser ref={this.gameRef} game={gameInstance} />}
+        </div>
+        <div className="gamepage__buttons">
+          <Link to="/">
+            <img
+              className="gamepage__button"
+              src={homeButton}
+              alt="home button"
+            />
+          </Link>
+          <label htmlFor="invisiblecheck">
+            <img
+              className="gamepage__button"
+              src={infoButton}
+              alt="back button"
+            />
+          </label>
+          <input
+            className="gamepage__invisiblecheck"
+            type="checkbox"
+            name="invisblecheck"
+            id="invisiblecheck"
+          />
+          <GameInstructions instructions={instructions} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default GamePage;
